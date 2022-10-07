13963ad14199b21ef47e0d563d7a5a9e35cb614f
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-10-06 23:16:32 -0700

    need search results page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./pages/Home.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import reducer, { initialState } from "./reducer.js";
+import { StateProvider } from "./StateProvider.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <StateProvider initialState={initialState} reduce={reducer}>
+      <App />
+    </StateProvider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
